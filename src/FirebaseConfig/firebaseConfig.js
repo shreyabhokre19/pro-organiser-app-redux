05c020ecebe5aacff5a1ce5730b0d63fef7e7aa0
shreyabhokre1 <52531472+shreyabhokre1@users.noreply.github.com>
2020-07-18 10:55:32 +0530

    done

---
+++ src/FirebaseConfig/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAdOi2iRyKM_7QkyLDBbDnTfKnfSrbSOeM",
+  authDomain: "pro-organiser-redux-7aeed.firebaseapp.com",
+  databaseURL: "https://pro-organiser-redux-7aeed.firebaseio.com",
+  projectId: "pro-organiser-redux-7aeed",
+  storageBucket: "pro-organiser-redux-7aeed.appspot.com",
+  messagingSenderId: "1021233983591",
+  appId: "1:1021233983591:web:17f8e427cff5623146e4c2",
+  measurementId: "G-YX5D851DVF",
+};
